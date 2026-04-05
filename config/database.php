7dbdc3673a853273b63f2b50e891e96f85094608
khoaphunsonac <khoa.trandang020704@hcmut.edu.vn>
2026-04-05 15:36:28 +0700

    chore: update database configuration and add public/storage to .dockerignore

--- config/database.php
+++ config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'disable',
+            'sslmode' => env('DB_SSLMODE', 'require'),
             'endpoint' => env('DB_ENDPOINT_ID'),
         ],
 
